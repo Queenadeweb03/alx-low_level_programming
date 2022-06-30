16da62960ad0f8329501cbd19f280b31e0c5005f
Queenadeweb03 <adelekeadeola1430@gmail.com>
2022-06-30 03:16:38 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#!/bin/bash
 include <stdio.h>
 /**
- * Description: main - prints out string to stdout.
- * Return: 0 if success.
+ * Description: main - prints out a string to stdout.
+ * return: 0 if success.
  */
 int main(void)
 {
-	puts("\"programmig is like building a multilingual puzzle");
+	put("\"programming is like building a multilingual puzzle");
 	return (0);
 }
