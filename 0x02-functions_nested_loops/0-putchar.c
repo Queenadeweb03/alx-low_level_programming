76b86c105306b4955bb465c2d999e34d0b5c1ea6
Queenadeweb03 <adelekeadeola1430@gmail.com>
2022-07-05 09:26:48 -0700

    Text

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-*main - prints putchar
-*Return: 0
-*/
+ *main - Entry point
+ *Return: 0 (Success)
+ */
 int main(void)
 {
 int i = 0;
-char text = "_putchar";
-while (text[i] !='\0')
+char text[] = "_putchar";
+while (text[i] != '\0')
 {
 _putchar(text[i]);
 i++;
