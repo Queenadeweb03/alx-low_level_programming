2805df32040a6aa9ab9f3f617a0c08b5a930b71c
Queenadeweb03 <adelekeadeola1430@gmail.com>
2022-07-06 04:08:34 -0700

    Task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ while (n >= 98)
 printf("%d", n);
 if (n != 98)
 {
-printf(",");
+printf(", ");
 }
 n--;
 }
@@ -24,7 +24,7 @@ while (n <= 98)
 printf("%d", n);
 if (n != 98)
 {
-printf(",");
+printf(", ");
 }
 n++;
 }
