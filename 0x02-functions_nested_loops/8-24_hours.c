781841728678fdd0ae677078e180b07b0eba77f0
Queenadeweb03 <adelekeadeola1430@gmail.com>
2022-07-06 06:53:19 -0700

    Hot fix 2

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,40 +1,21 @@
 #include "main.h"
 /**
- *jack_bauer - Prints every minute of the day of Jack Bauer,
- *starting from 00:00 to 23:59
- */
+ *jack_bauer - Prints every minute of the day of
+ *Jack Bauer, starting from 00:00 to 23:59.
+ **/
 void jack_bauer(void)
 {
-int maxMinutes = 1440; /* Minutes in 24 hours */
-int hourFirstDigit = 0; /* The first digit of the current hour */
-int hourSecondDigit = 0; /* The second digit of the current hour */
-int minuteFirstDigit = 0; /* The first digit of the current minute */
-int minuteSecondDigit = 0; /* The second digit of the current minute */
-int currentMinute = 0; /* The current minute */
-while (currentMinute < maxMinutes)
+int hour, minute;
+for (hour = 0; hour <= 23; hour++)
 {
-_putchar(hourFirstDigit + '0');	
-_putchar(hourSecondDigit + '0');
-_putchar(':'); /* Separator */
-_putchar(minuteFirstDigit + '0');
-_putchar(minuteSecondDigit + '0');
+for (minute = 0; minute <= 59; minute++)
+{
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
+_putchar(':');
+_putchar((minute / 10) + '0');
+_putchar((minute % 10) + '0');
 _putchar('\n');
-minuteSecondDigit++;
-if (minuteSecondDigit > 9)
-{
-minuteSecondDigit = 0;
-minuteFirstDigit++;
 }
-if (minuteFirstDigit > 5)
-{	
-minuteFirstDigit = 0;
-hourSecondDigit++;
-}
-if (hourSecondDigit > 9)
-{
-hourSecondDigit = 0;
-hourFirstDigit++;
-}
-currentMinute++;
 }
 }
